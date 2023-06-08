f0618b0c97b79771ad4f389ec9fe291732fc0637
Kyle <kylcheng@gmail.com>
2023-06-08 08:11:01 -0700

    Attempt to break

--- packages/analytics/amplify_analytics_pinpoint_dart/test/queued_item_store_test.dart
+++ packages/analytics/amplify_analytics_pinpoint_dart/test/queued_item_store_test.dart
@@ -1,17 +1,25 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+@Timeout(Duration(seconds: 70))
+
 import 'package:amplify_analytics_pinpoint_dart/src/impl/analytics_client/event_client/queued_item_store/dart_queued_item_store.dart';
 import 'package:amplify_analytics_pinpoint_dart/src/impl/analytics_client/event_client/queued_item_store/queued_item_store.dart';
+import 'package:amplify_core/amplify_core.dart';
 
 import 'package:test/test.dart';
+
+final AmplifyLogger _logger = AmplifyLogger.category(Category.analytics)
+    .createChild('QueuedItemStoreTest');
 
 void main() {
   late DartQueuedItemStore db;
 
   group('DartQueuedItemStore ', () {
     setUpAll(() {
+      final stopwatch = Stopwatch()..start();
       db = DartQueuedItemStore('/tmp');
+      _logger.info('\n\n=== SETUP ===\n${stopwatch.elapsed}');
     });
 
     tearDownAll(() async {
@@ -27,17 +35,29 @@ void main() {
     }
 
     test('writes values to storage', () async {
+      // try to fake an easy op to see init times
+      final stopwatch1 = Stopwatch()..start();
+      await db.getCount(1);
+      _logger
+          .info('\n\n=== WRITE ===\ninit time elapses\n${stopwatch1.elapsed}');
+
+      final stopwatch = Stopwatch()..start();
       const values = ['0', '1', '2', '3', '4', '5'];
       for (final value in values) {
         await db.addItem(value);
       }
+      _logger.info('\n\n=== WRITE ===\nvalues added\n${stopwatch.elapsed}');
 
       final readItems = await db.getCount(values.length);
       final readValues = readItems.map((e) => e.value);
       expect(readValues, equals(values));
+
+      _logger.info('\n\n=== WRITE ===\nfinished\n${stopwatch.elapsed}');
     });
 
     test('returns first n items in storage', () async {
+      final stopwatch = Stopwatch()..start();
+
       const values = ['0', '1', '2', '3', '4', '5'];
       for (final value in values) {
         await db.addItem(value);
@@ -46,6 +66,10 @@ void main() {
       final readItems = await db.getCount(3);
       final readValues = readItems.map((e) => e.value);
       expect(readValues, equals(values.sublist(0, 3)));
+
+      _logger.info(
+        '\n\nreturns first n items in storage\nfinished\n${stopwatch.elapsed}',
+      );
     });
 
     test(
@@ -74,6 +98,8 @@ void main() {
     });
 
     test('deletes first subset of stored items', () async {
+      final stopwatch = Stopwatch()..start();
+
       const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
       for (final value in values) {
         await db.addItem(value);
@@ -84,6 +110,10 @@ void main() {
       final readItems = await getAll();
       final readValues = readItems.map((e) => e.value);
       expect(readValues, values.sublist(3));
+
+      _logger.info(
+        '\n\ndeletes first subset of stored items\nfinished\n${stopwatch.elapsed}',
+      );
     });
 
     test('deletes middle subset of stored items', () async {
